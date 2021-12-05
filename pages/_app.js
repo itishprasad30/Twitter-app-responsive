c6078a1526abd9e845f35a71105dfa5cd4101d3d
itishprasad30 <itishprasad20@gmail.com>
2021-12-05 19:42:59 +0530

    all componets control by this

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import "tailwindcss/tailwind.css";
+import "../styles/global.css";
+
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <RecoilRoot>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />;
+      </SessionProvider>
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
