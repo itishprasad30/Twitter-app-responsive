69478d108e5307ebbba43add978b3ac5a34dbf69
itishprasad30 <itishprasad20@gmail.com>
2021-12-06 10:43:21 +0530

    login header title added

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,13 @@
 import { signIn } from "next-auth/react";
+import Head from "next/head";
 import Image from "next/image";
 
 function Login({ providers }) {
   return (
     <div className="flex flex-col items-center space-y-20 pt-48">
+      <Head>
+        <title>Login page</title>
+      </Head>
       <Image
         src="https://rb.gy/ogau5a"
         width={150}
